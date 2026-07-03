746f6a0c361eea06862575e707f2e12bfdbf3276
Ruaidhrí Campion <campioru@tcd.ie>
2026-07-03 11:02:04 +0100

    double -> float speed up

--- matmult_cblas.c
+++ matmult_cblas.c
@@ -13,19 +13,19 @@
 int main (int argc, char **argv)
 {
    int i, j, k;
-   double t_mult;
+   float t_mult;
    struct timespec ts, tf;
 
       // Allocate matrices and initialize A and B
 
-   int N = 1000;
-   double A[N][N], B[N][N], C[N][N];
+   int N = 10000;
+   float A[N][N], B[N][N], C[N][N];
 
    for( i = 0; i < N; i++ ) {
       for( j = 0; j < N; j++ ) {
-         A[i][j] = (double) (i+j);
-         B[i][j] = (double) (i*j + 1.0);
-         C[i][j] = (double) (0.0);
+         A[i][j] = (float) (i+j);
+         B[i][j] = (float) (i*j + 1.0);
+         C[i][j] = (float) (0.0);
       }
    }
 
@@ -33,12 +33,12 @@ int main (int argc, char **argv)
 
    clock_gettime(CLOCK_REALTIME, &ts);
 
-   cblas_dgemm( CblasRowMajor, CblasNoTrans, CblasNoTrans,
+   cblas_sgemm( CblasRowMajor, CblasNoTrans, CblasNoTrans,
                 N, N, N, 1.0, *A, N, *B, N, 1.0, *C, N );
 
    clock_gettime(CLOCK_REALTIME, &tf);
-   t_mult =  (double) ( tf.tv_sec - ts.tv_sec );
-   t_mult += (double) (tf.tv_nsec - ts.tv_nsec) * 1e-9;
+   t_mult =  (float) ( tf.tv_sec - ts.tv_sec );
+   t_mult += (float) (tf.tv_nsec - ts.tv_nsec) * 1e-9;
 
    printf( "Multiplying 2 matrices of size %d took %lf seconds\n", N, t_mult);
 }
